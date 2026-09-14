76690d63e8c0c437971d580779ed8c940339c8d0
Vansh Agrawal <vansh1909agrawal@gmail.com>
2026-09-14 23:11:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth from End of Linked List/kth-from-end-of-linked-list.java
@@ -0,0 +1,28 @@
+/* Structure of Linked List Node
+class Node {
+	int data;
+	Node next;
+	Node(int x) {
+		data = x;
+		next = null;
+	}
+} */
+
+class Solution {
+	public int getKthFromLast(Node head, int k) {
+		// code here
+		Node slow = head;
+		Node fast = head;
+		for (int i = 0; i < k  ; i++) {
+			if (fast == null) {
+				return -1; }
+				fast = fast.next;
+			}
+			while (fast != null) {
+				
+				slow = slow.next;
+				fast = fast.next;
+			}
+			return slow.data;
+		}
+	}
